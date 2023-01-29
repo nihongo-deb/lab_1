747693a168c847048c43a2b113210823af6f4eed
nihongo-deb <dal34@tpu.ru>
2023-01-29 14:41:36 +0700

    create class ImageNegativeCompression for lab (read image)

--- src/main/java/com/nihongo_deb/App.java
+++ src/main/java/com/nihongo_deb/App.java
@@ -1,13 +1,15 @@
 package com.nihongo_deb;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+
     }
 }
--- src/main/java/com/nihongo_deb/ImageNegativeCompression.java
+++ src/main/java/com/nihongo_deb/ImageNegativeCompression.java
@@ -1,7 +1,45 @@
-package com.nihongo_deb;/**
- *
- * @project lab_1
+package com.nihongo_deb;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
  * @author KAWAIISHY
+ * @project lab_1
  * @created 28.01.2023
- */public class ImageNegativeCompression {
+ */
+public class ImageNegativeCompression {
+    private BufferedImage image;
+
+    public void readeImageFromResources(String name) {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try (InputStream io = classLoader.getResourceAsStream(name)){
+            image = ImageIO.read(io);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readImageFromAbsoluteFilePath(String absoluteImageFilePath){
+        File file = new File(absoluteImageFilePath);
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+
 }
